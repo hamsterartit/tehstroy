3eab9ef49b7f179bbb1b44a6e5c4570cc5af1c0d
Kristina <kristina.pt@velox.biz>
2021-08-11 23:18:28 +0600

    added services

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 /* global $ */
 
 $(document).ready(function() {
+    /* Banner slider */
     if($(".js-banner-slider")) {
         new Swiper(".js-banner-slider", {
             pagination: {
@@ -10,10 +11,22 @@ $(document).ready(function() {
         });
     }
 
+    /* Nav mobile button*/
     $('.js-nav-btn').click(function() {
         $(this).toggleClass('_active');
         $('.js-nav-body').toggleClass('_active');
         $('.js-header').toggleClass('_active');
         $('body').toggleClass('locked');
+    });
+
+    /* Tabs */
+    $('.js-tab-btn').click(function(e) {
+        e.preventDefault();
+        const $btn = $(e.currentTarget);
+        $('.js-tab-btn').removeClass('_active');
+        $('.js-tab-content').removeClass('_active');
+        const $contentId = $btn.attr('data-tab');
+        $btn.addClass('_active');
+        $("[data-content-tab='" + $contentId + "']").addClass('_active');
     });
 });
